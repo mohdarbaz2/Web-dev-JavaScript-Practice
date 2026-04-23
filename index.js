4b1176db698fd7d71a8560a4fb1d5d1d7e48f861
Mohd Arbaz Khan <122942234+mohdarbaz2@users.noreply.github.com>
2026-04-23 10:52:41 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// console.log("Hello JavaScript.....")
+
+
+//How to accept user input
+
+//1. Easy way= window prompt
+//2. PROFESSIONAL WAY = HTML textbox
+
+let username;
+
+username = window.prompt("What's your username");
+
+console.log(username)

2de79fdb37ce72802d8b11a0d3d052e346c512b9
Mohd Arbaz Khan <122942234+mohdarbaz2@users.noreply.github.com>
2026-04-23 11:06:08 +0530

    done with this

--- index.js
+++ index.js
@@ -5,9 +5,10 @@
 
 //1. Easy way= window prompt
 //2. PROFESSIONAL WAY = HTML textbox
-
 let username;
 
-username = window.prompt("What's your username");
-
-console.log(username)
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    // console.log(username);
+    document.getElementById("myH1").textContent =`Hello ${username}`;
+}
